ac31269f2d75c63bb8150b9683c640898180514e
mnilogi <75237482+mnilogi@users.noreply.github.com>
2021-01-24 15:32:43 +0530

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,30 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
-
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
-
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
-
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
-
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
-
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
-
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+void Process::Pid(int n){
+    pid = n;
+}
+int Process::Pid() const{ 
+    return pid;
+}
+float Process::CpuUtilization() const{
+    return(LinuxParser::cpu_util(Pid()));    
+}
+string Process::Command() { 
+    return(LinuxParser::Command(Pid()));
+ }
+string Process::Ram() { 
+    return(LinuxParser::Ram(Pid()));
+}
+string Process::User() { 
+    return(LinuxParser::User(LinuxParser::Uid(Pid())));
+}
+long int Process::UpTime() const{ 
+    return(LinuxParser::UpTime(Pid()));
+}
