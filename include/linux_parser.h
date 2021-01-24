67b532cdf986685e84e35e3d6e616162f7fb4691
mnilogi <75237482+mnilogi@users.noreply.github.com>
2021-01-24 15:28:31 +0530

    Update linux_parser.h

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -40,17 +40,13 @@ enum CPUStates {
   kGuest_,
   kGuestNice_
 };
-std::vector<std::string> CpuUtilization();
-long Jiffies();
-long ActiveJiffies();
-long ActiveJiffies(int pid);
-long IdleJiffies();
+float cpu_util(int pid);
 
 // Processes
 std::string Command(int pid);
 std::string Ram(int pid);
 std::string Uid(int pid);
-std::string User(int pid);
+std::string User(std::string uid);
 long int UpTime(int pid);
 };  // namespace LinuxParser
 
